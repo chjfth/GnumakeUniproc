f433ec3554c0dca236693b46e5d558ac03b91412
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2008-11-29 07:32:15 +0000

    Add gtktetris example using PI_use_pch_gcc plugin.
    A fix to PI_use_pch_gcc plugin.
    
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@701 429491bc-2e6e-d343-9161-6a1f6e34ed84

---
+++ GMU-examples/linux/gtk-tetris/gtktetris-gmu/tetris-pch.h
@@ -0,0 +1,14 @@
+#ifndef __tetris_pch_h_
+#define __tetris_pch_h_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <gdk/gdkkeysyms.h>
+#include <gtk/gtk.h>
+
+#endif
