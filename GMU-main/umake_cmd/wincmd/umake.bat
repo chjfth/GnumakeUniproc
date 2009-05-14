c8ecd48f72b999c16c6f833fbdcb12b5706d8331
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2009-05-14 08:04:41 +0000

    Improvement! Use setlocal command to isolate env-var setting from outer environment. So, for umakeD, umakeU, umakeUD,, gmp_bc_DEBUG=1, gmp_bc_UNICODE=1 is now set as env-var, not command-line parameters to make.exe . The bad thing about setting them as command-line parameter is: It is impossible to override them in user makefile without explicitly writing *override somevar=somevalue* .
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@772 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- GMU-main/umake_cmd/wincmd/umake.bat
+++ GMU-main/umake_cmd/wincmd/umake.bat
@@ -1,41 +1,5 @@
 @echo off
-REM 'dir > null' resets the internal %ERRORLEVEL% to 0. We must do this because some internal cmd like "set" does not reset "ERRORLEVEL".
-dir > nul
-REM Delete GnumakeUniproc start-up signiture file
-IF EXIST _MainPrjBuildStart.gmu.ckt DEL _MainPrjBuildStart.gmu.ckt
-IF EXIST _MainPrjBuildStart.gmu.ckt GOTO ErrorDelGmuSig
+REM 'dir > null' resets the internal %ERRORLEVEL% to 0. We must do this because some internal cmd like "ECHO", "SET" or "IF" does not reset "ERRORLEVEL", and if so, a non-zero %ERRRORLEVEL% from CMD environment will strike ``IF NOT %ERRORLEVEL% == 0 GOTO ErrorRename'' thus cause failure. [2009-05-14] Today, use setlocal instead of ``dir>null''to kill two birds with one stone.
+setlocal
 
-REM set GMU_PATH_PRE_SAVE=%PATH%
-	rem For the old sh.exe, The PATH value will change weirdly in sh.exe
-
-IF "x%gmu_LOG_OUTPUT_FILENAME%x" == "xx" goto NoLogOutput
-
-REM Backup old log file by renaming
-SET gmu_LOG_OUTPUT_FILENAME_bak=%gmu_LOG_OUTPUT_FILENAME%.bak
-
-IF EXIST %gmu_LOG_OUTPUT_FILENAME_bak% DEL %gmu_LOG_OUTPUT_FILENAME_bak%
-IF EXIST %gmu_LOG_OUTPUT_FILENAME_bak% goto ErrorDelLogBak
-
-IF EXIST %gmu_LOG_OUTPUT_FILENAME% REN %gmu_LOG_OUTPUT_FILENAME% %gmu_LOG_OUTPUT_FILENAME_bak%
-IF NOT %ERRORLEVEL% == 0 GOTO ErrorRename
-
-make%gmu_MAKEEXESUFFIX% %* 2>&1 | tee "%gmu_LOG_OUTPUT_FILENAME%"
-goto END
-
-:NoLogOutput
-make%gmu_MAKEEXESUFFIX% %*
-GOTO END
-
-:ErrorDelGmuSig
-echo Error from %0: Cannot delete GnumakeUniproc start-up signature file(_MainPrjBuildStart.gmu.ckt) in current dir. GnumakeUniproc will not work.
-goto END
-
-:ErrorDelLogBak
-echo Error from %0: Cannot delete backup log-file(%gmu_LOG_OUTPUT_FILENAME_bak%) .
-goto END
-
-:ErrorRename
-echo Error from %0: Cannot Rename %gmu_LOG_OUTPUT_FILENAME% to %gmu_LOG_OUTPUT_FILENAME_bak% .
-goto END
-
-:END
+call %~dp0umake-share.bat
