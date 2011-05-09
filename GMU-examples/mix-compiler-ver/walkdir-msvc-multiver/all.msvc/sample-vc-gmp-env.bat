7eec6312fe06f8db10b6f68d067793bd2a83cc37
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2011-05-09 06:43:10 +0000

    Update walkdir-msvc-multiver for GnumakeUniproc 0.97 .
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1005 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- GMU-examples/mix-compiler-ver/walkdir-msvc-multiver/all.msvc/sample-vc-gmp-env.bat
+++
@@ -1,3 +0,0 @@
-set gmp_MSVC60_ROOT=C:/Program Files/Microsoft Visual Studio
-set gmp_MSVC60_PSDK_ROOT=C:/Program Files/Microsoft SDK
-set gmp_MSVC80_ROOT=C:/Program Files/Microsoft Visual Studio 8
---
+++ GMU-examples/mix-compiler-ver/walkdir-msvc-multiver/sample-vc-gmp-env.bat
@@ -0,0 +1,3 @@
+set gmp_CENV_ROOT_msvc_vc60=C:/Program Files/Microsoft Visual Studio
+set gmp_CENV_PSDK_ROOT_msvc_vc60=C:/Program Files/Microsoft SDK
+set gmp_CENV_ROOT_msvc_vc80=C:/Program Files/Microsoft Visual Studio 8
