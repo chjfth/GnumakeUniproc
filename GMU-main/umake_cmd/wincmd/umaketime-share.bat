95b3592f831e04532e49f4f190910608ae85cf26
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2011-06-02 13:15:40 +0000

    Fix time display.
    Before this fix, 1040ms is displayed as "Gmu seconds used: 1.40" . Now, the correct 1.04 .
    
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1048 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- GMU-main/umake_cmd/wincmd/umaketime-share.bat
+++ GMU-main/umake_cmd/wincmd/umaketime-share.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal
-:Thanks to jeb from http://stackoverflow.com/questions/4487100/how-can-i-use-a-windows-batch-file-to-measure-the-performance-of-console-applicat
 set "startTime=%time%"
 
 call %~dp0umake-share.bat %*
@@ -8,10 +7,14 @@ call %~dp0umake-share.bat %*
 set "stopTime=%time%"
 call :timeDiff diff startTime stopTime
 set /A _sec_=%diff% / 1000
-set /A _msec_=%diff% %% 1000
-echo Gmu seconds used: %_sec_%.%_msec_%
+set /A _hsec_=%diff%/10 %% 100
+call :strlen _hsec_ hsdigits
+if %hsdigits% == 1 set _hsec_=0%_hsec_%
+echo Gmu seconds used: %_sec_%.%_hsec_%
 goto :eof
 
+
+:Thanks to jeb from http://stackoverflow.com/questions/4487100/how-can-i-use-a-windows-batch-file-to-measure-the-performance-of-console-applicat
 :timeDiff
 setlocal
 call :timeToMS time1 "%~2"
@@ -34,3 +37,24 @@ FOR /F "tokens=1,2,3,4 delims=:,.^ " %%a IN ("!%~2!") DO (
   set %~1=%ms%
   goto :eof
 )
+
+::Thanks to http://www.dostips.com/?t=Function.strLen
+:strLen string len -- returns the length of a string
+::                 -- string [in]  - variable name containing the string being measured for length
+::                 -- len    [out] - variable to be used to return the string length
+:: Many thanks to 'sowgtsoi', but also 'jeb' and 'amel27' dostips forum users helped making this short and efficient
+:$created 20081122 :$changed 20101116 :$categories StringOperation
+:$source http://www.dostips.com
+(   SETLOCAL ENABLEDELAYEDEXPANSION
+    set "str=A!%~1!"&rem keep the A up front to ensure we get the length and not the upper bound
+                     rem it also avoids trouble in case of empty string
+    set "len=0"
+    for /L %%A in (12,-1,0) do (
+        set /a "len|=1<<%%A"
+        for %%B in (!len!) do if "!str:~%%B,1!"=="" set /a "len&=~1<<%%A"
+    )
+)
+( ENDLOCAL & REM RETURN VALUES
+    IF "%~2" NEQ "" SET /a %~2=%len%
+)
+EXIT /b
