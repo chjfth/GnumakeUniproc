4b3c1e46cef00123f35c333aaa141cfd3e31b851
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2014-11-19 08:17:36 +0000

    minor
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1187 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- gmupath.bat
+++ gmupath.bat
@@ -5,11 +5,11 @@
 
 @set _gmuex1=%tmpbatdir%\GMU-examples\common\walkdir\make-all
 
-@set gv1=gmu_DO_SHOW_VERBOSE=1 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
-@set gv2=gmu_DO_SHOW_VERBOSE=2 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
+set gv1=gmu_DO_SHOW_VERBOSE=1 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
+set gv2=gmu_DO_SHOW_VERBOSE=2 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
 
 @set tmpdir=%tmpbatdir%\GMU-main\umake_cmd\wincmd
 @REM -- No need to add %tmpbatdir%\MinGW2\bin here, it will be added in _gmuenv.bat
 @PATH=%tmpdir%;%PATH%
 
-echo GMU command PATH added: %tmpdir% 
+@echo GMU command PATH added: %tmpdir% 
