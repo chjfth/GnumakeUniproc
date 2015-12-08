bb8bbe55d89b228f3c06cb16a009cf7f0c1780c2
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2015-12-08 00:42:45 +0000

    comment
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1213 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- GMU-examples/oneshot/msvc-exe/domake.bat
+++ GMU-examples/oneshot/msvc-exe/domake.bat
@@ -3,4 +3,15 @@
 REM Set gmp_COMPILER_ID to anything you like
 set gmp_COMPILER_ID=myvc
 
+
+REM set CURPATH=%CD%
+REM gmp_ud_list_CUSTOM_COMPILER_CFG=%CURPATH:\=/%
+rem		Make current abspath / separated. 
+rem		You may wish to define gmp_ud_list_CUSTOM_COMPILER_CFG if you have more than one project
+rem		sharing the same set of compiler_config.mki .
+rem		Example: If 
+rem			gmp_ud_list_CUSTOM_COMPILER_CFG=D:/myprj
+rem		you should prepare accordingly:
+rem			D:/myprj/myvc/compiler_config.mki
+
 umake %*
