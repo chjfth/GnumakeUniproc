47929dabf2d6e785fd5d89fb89cda808aabc3d46
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2016-01-14 03:11:20 +0000

    Put wincmd to rear of PATH. I think it provides more compatibility.
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1238 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- gmupath.bat
+++ gmupath.bat
@@ -3,15 +3,12 @@
 
 @SET NLSSVN=https://nlssvn/svnreps
 
-@set _gmuex1=%tmpbatdir%\GMU-examples\common\walkdir\make-all
-
 set gv1=gmu_DO_SHOW_VERBOSE=1 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
 set gv2=gmu_DO_SHOW_VERBOSE=2 gmu_DO_SHOW_COMPILE_CMD=1 gmu_DO_SHOW_LINK_CMD=1
 
 @set tmpdirgmu=%tmpbatdir%\GMU-main\umake_cmd\wincmd
-@REM -- No need to add %tmpbatdir%\MinGW2\bin here, it will be added in _gmuenv.bat
-@PATH=%tmpdirgmu%;%PATH%
+@PATH=%PATH%;%tmpdirgmu%
+@echo GMU command PATH added: %tmpdirgmu% 
 
 @call gmu-goody.bat
 
-@echo GMU command PATH added: %tmpdirgmu% 
