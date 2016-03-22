16e62757efeb0563390fc866f2fd49a3cca06e45
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2016-03-22 09:35:14 +0000

    temp.
    Introducing cidver-mapping.ini
    
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1323 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- extras/scripts/scalacon-get-sdkin.py
+++ extras/scripts/scalacon-get-sdkin.py
@@ -37,22 +37,25 @@ NLSSVN=https://nlssvn/svnreps
 gmb_dirname_sdkin=sdkin
 gmb_dirname_sdkout=sdkout
 
-[common-include]
+[sdkin.common-include]
 svndatetime= 2016-03-05 23:00
 svnurl=      %(NLSSVN)s/CommonLib/common-include/trunk
 localdir=    %(gmb_dirname_sdkin)s/include
+want-cidver-mapping=1
 ; localdir is relative to this INI file.
 
-[mmsnprintf]
+[sdkin.mm_snprintf]
 svndatetime= 2016-03-06 13:39:43
 svnurl=      %(NLSSVN)s/scalacon/sdkbin/mmsnprintf/4.2.0
 localdir=    %(gmb_dirname_sdkin)s
+want-cidver-mapping=1
 
-[self]
+[self.0]
 svndatetime= 2016-03-11 11:44:00
 svnurl=      %(NLSSVN)s/scalacon/sdkbin/IUartBasic/2.1.6
 localdir=    %(gmb_dirname_sdkout)s
-	
+want-cidver-mapping=1
+
 
 === <<< INI 格式范例
 
@@ -60,12 +63,14 @@ localdir=    %(gmb_dirname_sdkout)s
 
 [DEFAULT] 中定义的条目会被同名环境变量覆盖，这个覆盖动作是在本 .py 代码中实现的。
 
-[self] 小节，指示自己这个 SDK 的最新发布包，该内容默认将被取出到 sdkout 子目录中。
-[self] 小节的内容仅仅在传递了 --self 时才会去获取。注意，进行 make-sdk-all 构建后，
+[self.xxx] 小节，指示自己这个 SDK 的最新发布包，该内容默认将被取出到 sdkout 子目录中。
+[self.xxx] 小节的内容仅仅在传递了 --self 时才会去获取。注意，进行 make-sdk-all 构建后，
 sdkout 子目录中的内容会被覆盖（除非 make-sdk-all 选取不同的 SDK output 目录）。
+可以有多个 self. 小节来指示 self 来自多处，比如，将 [self.Windows] 和 [self.Linux] 。
 
 
-其他的小节，每一个小节对应一个 SDK 条目，小节名称可以任意命名，跟 SDK 本身并无相关性。
+其他的小节，若小节名以 sdkin. 起始，则此小节对应一个 SDK 条目，sdkin. 后的子串
+可以任意命名，跟 SDK 本身并无相关性。
 
 svndatetime 
 	指出抓取 svn 仓库中哪个时间点的内容，将用作 svn export 的 peg 参数。
@@ -79,6 +84,27 @@ localdir
 	将对应本地文件 $/sdkin/include/ps_TCHAR.h 
 	（此句用 $ 代表此 INI 所在的目录）
 	注意 sdkin 实际上由环境变量 gmb_dirname_sdkin 控制。
+
+额外行为：
+
+【cidver-mapping】 对于一个小节，在 want-cidver-mapping=1 的情况下，启动 cidver-mapping 行为。
+
+cidver 是指 vc60, vc80, vc100x64, gcc4.8_i686 这样的字眼，不同的 cidver 意味着用户可以使用
+不同版本的编译器来构建自己的工程。 一个 cidver 将呈现为 $/sdkin 中的一个子目录名。
+
+$/sdkin 中最终呈现的 cidver ，以及 cidver 中的内容由四层信息叠加而成。
+
+(1) 输入库(SDKIN)提供的实质 cidver 。比如，某个输入库有用 vc80 进行编译，则它实质提供 vc80 。
+(2) 输入库自带的 cidver-mapping.ini 指示的映射所产出的 cidver 。
+(3) 库用户自身 get-sdkin.ini 中 implicit-cidver-mapping= 指示的隐式映射。隐式的意思是，
+	当 (1)(2) 没有提供所需的 cidver 时，隐式映射起作用。
+(4) 库用户自身 get-sdkin.ini 中 explicit-cidver-mapping= 指示的强制映射。强制的意思是，
+	覆盖掉 (2),(3) 的映射方法，直接建立跟 (1) 的映射关系。若 (1) 中没有期望的 cidver 目标，
+	宣告失败。
+
+implicit-cidver-mapping= 和 explicit-cidver-mapping= 的取值格式：
+
+implicit-cidver-mapping = vc100(=vc80) vc100x64(=vc80x64)
 
 """
 
@@ -94,14 +120,16 @@ import ConfigParser # INI op
 
 version = "1.2"
 optdict = {}
-ini_filepath = '' # the config file(in INI format)
-ini_dir = ''
+g_ini_filepath = '' # the config file(in INI format)
+g_ini_dir = ''
 g_isforce = False
 g_is_do_self = False
 g_is_only_self = False
 g_self_dir_param = ''
 
 g_is_simulate = False
+
+DIRNAME_CACHE = 'sdkin-cidver-cache'
 
 def os_sep(hint=""):
 	# A more friendly os.sep .
@@ -134,13 +162,16 @@ def force_rmtree(dir):
 		shutil.rmtree(dir)
 	
 
-def pick_sections(iniobj, ini_dir):
+def pick_sections(iniobj, g_ini_dir):
 	sdknames = iniobj.sections()
 	for section in sdknames:
-		if section=='self' and not g_is_do_self:
+		if section.startswith('self.') and not g_is_do_self:
 			continue
-		if section!='self' and g_is_only_self:
+		if (not section.startswith('self.')) and g_is_only_self:
 			continue
+		
+		if not section.startswith('sdkin.'):
+			continue # unrecognized section name
 		
 		kvlist = iniobj.items(section)
 		dsection = dict(kvlist)
@@ -152,14 +183,30 @@ def pick_sections(iniobj, ini_dir):
 		else:
 			localdir = dsection['localdir']
 		
-		localdir_final = os.path.abspath( os.path.join(ini_dir, localdir))
+		localdir_final = os.path.abspath( os.path.join(g_ini_dir, localdir) )
 		yield section, dsection, localdir_final # dsection's d implies dict
+
+
+def getsdk_with_cidver_mapping(section, svnurl, svndatetime, localdir):
+	# section is section name
+	# [PENDING] g_isforce processing ........
+	cachedir = os.path.join(g_ini_dir, DIRNAME_CACHE. section
+	svncmd = 'svn export --force "%s@{%s}" "%s"'%(svnurl, svndatetime, cachedir)
+	print "Running cmd: \n  " + svncmd
+	
+	if g_is_simulate:
+		return 
+	
+	ret = subprocess.check_call(shlex.split(svncmd))
+	
+	# Apply four layer cid-mapping
+	# zzz
 
 def do_getsdks():
 	iniobj = ConfigParser.ConfigParser()
 	
 	try:
-		readret = iniobj.read(ini_filepath)
+		readret = iniobj.read(g_ini_filepath)
 		
 		# Override the values in [DEFAULT] section according to env-vars
 		kvpairs = iniobj.items('DEFAULT')
@@ -174,28 +221,43 @@ def do_getsdks():
 			iniobj.set('DEFAULT', key, value)
 		
 	except(MissingSectionHeaderError):
-		print 'Scalacon Error: "%s" does not look like a valid INI file.'%(ini_filepath)
+		print 'Scalacon Error: "%s" does not look like a valid INI file.'%(g_ini_filepath)
 		return 1
 
 	if not readret:
-		print 'Scalacon Error: The INI file "%s" does not exist or cannot be opened.'%(ini_filepath)
+		print 'Scalacon Error: The INI file "%s" does not exist or cannot be opened.'%(g_ini_filepath)
 		return 1
 	
 	if g_isforce:
 		# 删除目录动作要专门自己一个循环（而非在 fetch server content 循环中），
 		# 是考虑到各个 SDK 指定的 localdir 可能有重叠或父子关系，那将导致后一次循环的
 		# rmtree 将前一次循环取出的东西给删了。
-		for s,ds, localdir_final in pick_sections(iniobj, ini_dir):
+		for s,ds, localdir_final in pick_sections(iniobj, g_ini_dir):
 			print 'Removing old directory: %s'%(localdir_final)
 			if g_is_simulate:
 				continue
 			force_rmtree(localdir_final)
+		
+		# 删除 cidver cache 目录
+		force_rmtree( os.path.join(g_ini_dir, DIRNAME_CACHE) )
 
 	# For each SDK, fetch server content.
-	for section, dsection, localdir in pick_sections(iniobj, ini_dir):
+	for section, dsection, localdir in pick_sections(iniobj, g_ini_dir):
 
 		svnurl = dsection['svnurl']
 		svndatetime = dsection['svndatetime']
+		
+		try:
+			want_cidver_mapping = dsection['want-cidver-mapping']
+		except:
+			want_cidver_mapping = False
+		
+		if want_cidver_mapping:
+			getsdk_with_cidver_mapping(section, svnrul, svndatetime)
+		else:
+			getsdk_direct(section, svnrul, svndatetime)
+		
+###
 		print "SDK[%s] from %s"%(section, svnurl)
 		
 		if (not g_isforce) and os.path.exists(localdir):
@@ -219,7 +281,7 @@ def do_getsdks():
 	return 0
 
 def main():
-	global optdict, ini_filepath, ini_dir, g_isforce
+	global optdict, g_ini_filepath, g_ini_dir, g_isforce
 	global g_is_do_self, g_is_only_self, g_self_dir_param
 	global g_is_simulate
 
@@ -233,8 +295,8 @@ def main():
 		return 0;
 
 	if '--ini' in optdict:
-		ini_filepath = optdict['--ini']
-		ini_dir = os.path.split(ini_filepath)[0]
+		g_ini_filepath = optdict['--ini']
+		g_ini_dir = os.path.split(g_ini_filepath)[0]
 	else:
 		sys.stderr.write('Error: You need to assign an INI file describing the SDKs to get(--ini=xxx.ini).\n')
 		return 1
