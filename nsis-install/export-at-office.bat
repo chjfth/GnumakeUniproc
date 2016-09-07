a410a7a04ff1e762f68568cb776bec091f715746
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2016-09-07 05:24:36 +0000

    export-at-office.bat will make .exe if makensis.exe is in PATH.
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1443 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- nsis-install/export-at-office.bat
+++ nsis-install/export-at-office.bat
@@ -1,8 +1,10 @@
+@echo off
 REM If I'm in nlscan office, just call this bat to export ALL files necessary
 REM to pack a GnumakeUniproc Win32 release.
 REM
 REM 2010-01-12 Memo:
 REM I have to manually build new make 3.81 and win-bash exe into MinGW-binary for a complete Windows version release.
+@echo ON
 
 rd /s /q nsis-data
 
@@ -12,7 +14,19 @@ call export-gmu.bat https://nlssvn/svnreps/makingsys/GnumakeUniproc
 call export-gmu-bin.bat  https://nlssvn/BinaryRls/MinGW-binary
 @if ERRORLEVEL 1 goto ErrSvnError
 
-@echo All exports success.
+@where makensis > NUL
+@if ERRORLEVEL 1 ( 
+	echo.
+	echo Export done, but makensis.exe not found in path. No exe is generated.
+	exit /b 0
+) else (
+	makensis GMU-nsis.nsi
+)
+
+@if ERRORLEVEL 1 goto ErrSvnError
+
+@echo.
+@echo ==== SUCCESS ====
 @goto END
 
 :ErrSvnError
