c0315e32841cd17c76db403f91c8b2bf6140646f
chj <chj@429491bc-2e6e-d343-9161-6a1f6e34ed84>
2018-03-20 08:23:26 +0000

    Fix .bat due to GMU change some moment ago.
    Reason: gmu_DIR_ROOT env-var is not required to exist in user's env since around GMU 0.100 .
    
    git-svn-id: file:///mnt/i/1027-HOME-svnreps/nlscan-svnreps/makingsys/GnumakeUniproc/trunk@1603 429491bc-2e6e-d343-9161-6a1f6e34ed84

--- GMU-examples/windows/KeyView2/test-sync-uoal.bat
+++ GMU-examples/windows/KeyView2/test-sync-uoal.bat
@@ -1,2 +1,9 @@
 @REM Test using PI_sync_devoutput to sync gmi_SYDO_ud_SYNC_UserOutputAL_TO(i.e. gmp_up_USER_OUTPUT_AFTER_LINK)
-umake gmu_DO_SHOW_VERBOSE=2 gmp_u_list_PLUGIN_TO_LOAD_ENV_PRE="PI_sync_devoutput" gmi_SYDO_ud_SYNC_UserOutputAL_TO=%gmu_DIR_ROOT% gmi_SYDO_SHOW_COPY_CMD=1
+@echo off
+setlocal
+set batdir=%~dp0
+set batdir=%batdir:~0,-1%
+set batdir_fs=%batdir:\=/%
+
+@echo on
+umake gmu_DO_SHOW_VERBOSE=2 gmp_u_list_PLUGIN_TO_LOAD_ENV_PRE="PI_sync_devoutput" gmi_SYDO_ud_SYNC_UserOutputAL_TO=%batdir_fs% gmi_SYDO_SHOW_COPY_CMD=1
